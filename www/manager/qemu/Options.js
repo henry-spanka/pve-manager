a81fbc3c7548044acdea4b365762411af4a4df2d
Alexandre Derumier <aderumier@odiso.com>
2015-01-02 15:18:04 +0100

    Implement pendinggrid and pendingstore
    Signed-off-by: Alexandre Derumier <aderumier@odiso.com>

--- www/manager/qemu/Options.js
+++ www/manager/qemu/Options.js
@@ -1,6 +1,6 @@
 /*jslint confusion: true */
 Ext.define('PVE.qemu.Options', {
-    extend: 'PVE.grid.ObjectGrid',
+    extend: 'PVE.grid.PendingObjectGrid',
     alias: ['widget.PVE.qemu.Options'],
 
     initComponent : function() {
@@ -332,7 +332,7 @@ Ext.define('PVE.qemu.Options', {
 	};
 
 	Ext.applyIf(me, {
-	    url: "/api2/json/nodes/" + nodename + "/qemu/" + vmid + "/config",
+	    url: "/api2/json/nodes/" + nodename + "/qemu/" + vmid + "/pending",
 	    cwidth1: 170,
 	    tbar: [ edit_btn ],
 	    rows: rows,
